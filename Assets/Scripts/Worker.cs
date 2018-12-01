5ee9ae2f01106e981a514663429e04e42dc31848
Kegnarok <kegnarok.rm@gmail.com>
2018-12-01 16:43:55 +0100

    Added grid and worker pathfinding.

---
+++ Assets/Scripts/Worker.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Worker : MonoBehaviour {
+    public enum Job { Farmer, Builder, Breeder, Priest };
+    public Job job = Job.Farmer;
+    public float velocity = 3f;
+
+    List<Vector2Int> currentPath;
+    int currentPathPos;
+
+    public Vector2Int target;
+
+    // Start is called before the first frame update
+    void Start() {
+        currentPath = new List<Vector2Int>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // Update target if need be
+
+        // Detect if we achieved our goal
+        if (currentPathPos < currentPath.Count) {
+            Vector2Int target = currentPath[currentPathPos];
+            Vector2Int curPos = GridPos(transform.position);
+            if (curPos == target) {
+                currentPathPos++;
+            }
+        }
+
+        // Move to target
+        if (currentPathPos < currentPath.Count) {
+            Vector2Int target = currentPath[currentPathPos];
+            Debug.Log("Moving to " + RealPos(target).ToString() + " from " + transform.position.ToString());
+            Vector3 delta = RealPos(target) - transform.position;
+            delta = delta * Mathf.Min(1, velocity * Time.deltaTime / delta.magnitude);
+            transform.position += delta;
+        }
+    }
+
+    Vector2Int GridPos(Vector3 pos) {
+        return new Vector2Int((int)pos.x, (int)pos.z);
+    }
+
+    Vector3 RealPos(Vector2Int pos) {
+        return new Vector3(pos.x + 0.5f, 0f, pos.y + 0.5f);
+    }
+
+    void moveTo(Vector2Int target) {
+        Vector2Int origin = GridPos(transform.position);
+        Debug.Log("origin: " + origin.ToString() + " target: " + target.ToString());
+        currentPath = WorldGrid.instance.Smooth(WorldGrid.instance.Path(origin, target));
+        Debug.Log("New path: " + String.Join(";", currentPath.ConvertAll(v => v.ToString()).ToArray()));
+        currentPathPos = 0;
+    }
+
+    private void OnValidate() {
+        if (WorldGrid.instance != null) {
+            moveTo(target);
+        }
+    }
+}
--- Assets/Scripts/WorldGrid.cs
+++ Assets/Scripts/WorldGrid.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,15 +9,115 @@ public class WorldGrid : MonoBehaviour {
 
     public int width;
     public int height;
-    
-    private MonoBehaviour[] cells;
-    
+
+    public static float roadFactor = 3f;
+
+    public struct Cell {
+        public bool isObstacle;
+        public bool isRoad;
+    }
+
+    public struct Position : IComparable<Position> {
+        public Vector2Int pos;
+        public Vector2Int father;
+        public float distanceFromOrigin;
+        public float estimatedTotalDistance;
+
+        public Position(Vector2Int pos, Vector2Int father, float dOrigin, Vector2Int target) {
+            this.pos = pos;
+            this.father = father;
+            distanceFromOrigin = dOrigin;
+            Vector2Int delta = target - pos;
+            estimatedTotalDistance = dOrigin + (Mathf.Abs(delta.x) + Mathf.Abs(delta.y)) / roadFactor;
+        }
+
+        public int CompareTo(Position other) {
+            return this.estimatedTotalDistance.CompareTo(other.estimatedTotalDistance);
+        }
+    }
+
+    public struct PathInfo {
+        public Vector2Int father;
+        public float distance;
+        public bool visited;
+
+        public PathInfo(Vector2Int father, float distance) {
+            this.father = father;
+            this.distance = distance;
+            visited = true;
+        }
+    }
+
+    public struct Neighbor {
+        public Vector2Int pos;
+        public float distance;
+
+        public Neighbor(Vector2Int pos, float distance) {
+            this.pos = pos;
+            this.distance = distance;
+        }
+    }
+
+    public Cell[,] cells;
+
     private void Start() {
-        if(instance != null) Debug.LogError("Multiples instances of Grid");
+        if (instance != null) Debug.LogError("Multiples instances of Grid");
         instance = this;
-        cells = new MonoBehaviour[width * height];
+        cells = new Cell[height, width];
     }
 
     private void Update() {
+    }
+
+    public List<Neighbor> Neighbors(Vector2Int pos) {
+        List<Neighbor> neighbors = new List<Neighbor>(8);
+
+        for (int y = -1; y <= 1; y++) {
+            for (int x = -1; x <= 1; x++) {
+                Vector2Int delta = new Vector2Int(x, y);
+                Vector2Int next = pos + delta;
+                float dist = delta.magnitude;
+                if (next.x >= 0 && next.x < width && next.y >= 0 && next.y < height && dist != 0 && !cells[next.y, next.x].isObstacle) {
+                    neighbors.Add(new Neighbor(next, dist));
+                }
+            }
+        }
+
+        return neighbors;
+    }
+
+    public List<Vector2Int> Path(Vector2Int origin, Vector2Int target) {
+        PathInfo[,] path = new PathInfo[height, width];
+        PriorityQueue<Position> toVisit = new PriorityQueue<Position>();
+        toVisit.Enqueue(new Position(origin, origin, 0, target));
+
+        while (toVisit.Count() > 0) {
+            Position c = toVisit.Dequeue();
+            if (path[c.pos.y, c.pos.x].visited) {
+                continue;
+            }
+            path[c.pos.y, c.pos.x] = new PathInfo(c.father, c.distanceFromOrigin);
+            if (c.pos == target) {
+                break;
+            }
+            foreach (Neighbor n in Neighbors(c.pos)) {
+                Position d = new Position(n.pos, c.pos, c.distanceFromOrigin + n.distance / (cells[c.pos.y, c.pos.x].isRoad && cells[n.pos.y, n.pos.x].isRoad ? 1 : roadFactor), target);
+                toVisit.Enqueue(d);
+            }
+        }
+
+        List<Vector2Int> positions = new List<Vector2Int>();
+        Vector2Int cur = target;
+        while (cur != origin) {
+            positions.Add(cur);
+            cur = path[cur.y, cur.x].father;
+        }
+        positions.Reverse();
+
+        return positions;
+    }
+
+    public List<Vector2Int> Smooth(List<Vector2Int> path) {
+        return path;
     }
 }
