31b1cf8eb3b49aa82cc3cbf3ac1e3a713e43076d
Elodie <diidiie159@gmail.com>
2018-12-01 22:11:22 +0100

    Buildings layout

--- Assets/Scripts/BuildingSelector.cs
+++ Assets/Scripts/BuildingSelector.cs
@@ -1,24 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class BuildingSelector : MonoBehaviour {
     public bool selected;
     public string selector;
+    public Camera camera;
+    public LayerMask mask;
+    public Transform cube;
+    private Transform c;
 
     // Start is called before the first frame update
     void Start() {
         selected = false;
         selector = null;
+        c = Instantiate(cube);
     }
 
     // Update is called once per frame
     void Update() {
-        Debug.Log(selected);
+        if (selected) {
+            var ray = camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            Physics.Raycast(ray, out hit, 100, mask);
+            c.position = hit.point; 
+            
+            if (Input.GetMouseButtonDown(0)) {
+                c.position = hit.point;
+                c = Instantiate(cube);
+                UpdateSelection(null, false);
+                
+            }
+        }
     }
 
-    public void UpdateSelection(string nameselector, bool select) {
+    public void UpdateSelection(string nameSelector, bool select) {
         selected = select;
-        selector = nameselector;
+        selector = nameSelector;
     }
 }
