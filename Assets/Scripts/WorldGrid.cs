67bdfd44c04cf32bede11cb82c6c02f288c7fe84
VengeurK <vengeurk@gmail.com>
2018-12-01 13:13:08 +0100

    A* lib

---
+++ Assets/Scripts/WorldGrid.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGrid : MonoBehaviour {
+
+    public static WorldGrid instance;
+
+    public int width;
+    public int height;
+    
+    private MonoBehaviour[] cells;
+    
+    private void Start() {
+        if(instance != null) Debug.LogError("Multiples instances of Grid");
+        instance = this;
+        cells = new MonoBehaviour[width * height];
+    }
+
+    private void Update() {
+    }
+}
