38996d3652d4cb2f829e2ba5147a260afe1f3a30
Elodie <diidiie159@gmail.com>
2018-12-02 13:33:56 +0100

    Canevas

---
+++ Assets/Scripts/Canevas/ChangeInformation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeInformation : MonoBehaviour {
+    public Text txt; 
+
+    public void CInformation(BuildingSelector selector) {
+        
+    }
+}
